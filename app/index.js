0d11ea0a164fecd6d12a17e0abcf0eeab833a252
anaconda0905 <conda0905@gmail.com>
2020-06-09 10:38:49 +0200

    Initialize

---
+++ app/index.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import { store, persistor } from "app/store";
+import { StatusBar } from "react-native";
+import { BaseColor } from "@config";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import App from "./navigation";
+
+console.disableYellowBox = true;
+
+export default class index extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    StatusBar.setBackgroundColor(BaseColor.primaryColor, true);
+  }
+
+  render() {
+    return (
+      <Provider store={store}>
+        {/* makes the redux store available to any nested components that have been wrapped in the connect() function */}
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
+    );
+  }
+}
